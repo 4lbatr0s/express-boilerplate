80fd0de428a625b9aea4746e7f973b51afcb34ab
4lbatr0s <82971869+4lbatr0s@users.noreply.github.com>
2023-03-01 21:19:26 +0300

    Update index.js

--- v1/src/api-routes/index.js
+++ v1/src/api-routes/index.js
@@ -1,13 +1,7 @@
-import ProjectRoutes from "./Projects.js";
 import UserRoutes from "./Users.js";
-import SectionRoutes from "./Sections.js";
-import TaskRoutes from "./Tasks.js";
 
 export default function loadRoutes(app) {
-  app.use("/projects", ProjectRoutes);
   app.use("/users", UserRoutes);
-  app.use("/sections", SectionRoutes);
-  app.use("/tasks", TaskRoutes);
 }
 
 // export default {ProjectRoutes, UserRoutes, SectionRoutes}
